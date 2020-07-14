bd75f7ee0e7a7ed458d0dcadc0ac50d784d380f8
Nick Mardon <nickmardondigital@gmail.com>
2020-07-14 10:20:08 -0700

    pseudo coded if else chain

--- data.sql
+++ data.sql
@@ -15,7 +15,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NOT NULL,
-  manager_id INT,
+  manager_id INT DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
@@ -40,7 +40,7 @@ INSERT INTO employee (first_name,last_name,role_id,manager_id)
 values("Bradley","Davis",1,3);
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
 values("Mark","Foreman",2,4);
-INSERT INTO employee (first_name,last_name,role_id,manager_id)
+INSERT INTO employee (first_name,last_name,role_id)
 values("Mike","Sanchez",4);
 
 
--- index.js
+++ index.js
@@ -30,5 +30,34 @@ var connection = mysql.createConnection({
           name: "userChoice"
       }).then(function(answers){
           console.log(answers.userChoice);
+          if(answers.userChoice==="View all employees"){
+              // Make a get request to return all employees
+
+          }
+          if(answers.userChoice==="View all employees by department"){
+              // Make a get request to return employees based on department id
+              viewEmployees();
+          }
+          if(answers.userChoice==="View all employees by Manager"){
+              // Make a get request to return employees based on manager id
+          }
+          if(answers.userChoice==="Add Employee"){
+              // Ask user for first name, last name, role id, and manager id
+              // Make a post request that adds an employee to the employee table
+          }
+          if(answers.userChoice==="Remove Employee"){
+              // Provide user with list of employees
+              // Remove choosen employee
+          }
+          if(answers.userChoice==="Update Employee Manager"){
+              // Provide user with list of employees
+              // Provide user with list of mangers
+              // Change selected employees manager id based on choosen manager
+          }
+
       })
   }
+
+    function viewEmployees(){
+        
+    }
