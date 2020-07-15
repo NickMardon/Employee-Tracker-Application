97627788aec6e8c5341612944131a6191862aefa
Nick Mardon <nickmardondigital@gmail.com>
2020-07-14 19:06:31 -0700

    updating if else chain and initial prompt

--- index.js
+++ index.js
@@ -26,38 +26,51 @@ var connection = mysql.createConnection({
       inquirer.prompt({
           type: "list",
           message: "What would you like to do?",
-          choices: ["View all employees", "View all employees by department", "View all employees by Manager", "Add Employee", "Remove Employee", "Update Employee Manager"],
+          choices: ["View all employees", "View all departments", "View all Managers", "Add Employee", "Add a Department", "Add a Manager", "Update Employee Role", "Quit"],
           name: "userChoice"
       }).then(function(answers){
           console.log(answers.userChoice);
           if(answers.userChoice==="View all employees"){
               // Make a get request to return all employees
+              viewEmployees();
 
           }
-          if(answers.userChoice==="View all employees by department"){
+          if(answers.userChoice==="View all departments"){
               // Make a get request to return employees based on department id
-              viewEmployees();
+              viewDepartments();
           }
-          if(answers.userChoice==="View all employees by Manager"){
+          if(answers.userChoice==="View all Managers"){
               // Make a get request to return employees based on manager id
+              viewManagers()
           }
           if(answers.userChoice==="Add Employee"){
               // Ask user for first name, last name, role id, and manager id
               // Make a post request that adds an employee to the employee table
+              addEmployee();
           }
-          if(answers.userChoice==="Remove Employee"){
+          if(answers.userChoice==="Add a Department"){
               // Provide user with list of employees
               // Remove choosen employee
+              addDepartment();
           }
-          if(answers.userChoice==="Update Employee Manager"){
+          if(answers.userChoice==="Add a Manager"){
               // Provide user with list of employees
               // Provide user with list of mangers
               // Change selected employees manager id based on choosen manager
+              addManager();
+          }
+          if(answers.userChoice==="Update Employee Role"){
+            // update employee role with put request
+            updateEmployeeRole();
+        }
+          if(answers.userChoice==="Quit"){
+              // quit app
+              
           }
 
       })
   }
 
     function viewEmployees(){
-        
+
     }
