a1bb7aa465b81da6524a05cf0a8f92e3afb4a042
Nick Mardon <nickmardondigital@gmail.com>
2020-07-15 09:39:50 -0700

    employee constructor function working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const Employee = require('./constructors/employee');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -26,7 +27,7 @@ var connection = mysql.createConnection({
       inquirer.prompt({
           type: "list",
           message: "What would you like to do?",
-          choices: ["View all employees", "View all departments", "View all Managers", "Add Employee", "Add a Department", "Add a Manager", "Update Employee Role", "Quit"],
+          choices: ["View all employees", "View all departments", "View all Roles", "Add Employee", "Add a Department", "Add a Manager", "Update Employee Role", "Quit"],
           name: "userChoice"
       }).then(function(answers){
           console.log(answers.userChoice);
@@ -39,9 +40,9 @@ var connection = mysql.createConnection({
               // Make a get request to return employees based on department id
               viewDepartments();
           }
-          if(answers.userChoice==="View all Managers"){
+          if(answers.userChoice==="View all Roles"){
               // Make a get request to return employees based on manager id
-              viewManagers()
+              viewRoles()
           }
           if(answers.userChoice==="Add Employee"){
               // Ask user for first name, last name, role id, and manager id
@@ -85,42 +86,79 @@ var connection = mysql.createConnection({
         })
     }
 
-    function viewRole(){
+    function viewRoles(){
         connection.query("SELECT * FROM role",(err, data)=>{
             if (err) throw err;
             console.table(data);
         })
     }
 
-    // function addEmployee(){
-    //     inquirer.prompt([
-    //             {
-    //                 type: "input",
-    //                 message: "What it the first name of the employee you would like to add?",
-    //                 name: "firstName"
-    //             },
-    //             {
-    //                 type: "input",
-    //                 message: "What it the last name of the employee you would like to add?",
-    //                 name: "lastName"
-    //             },
-    //             {
-    //                 type: "input",
-    //                 message: "What it the role of the employee you would like to add?",
-    //                 name: "role"
-    //             },
-    //             {
-    //                 type: "input",
-    //                 message: "Who is the manager of the employee you would like to add?",
-    //                 name: "manager"
-    //             }
-    //         ]).then(function(res){
-    //             console.log(res);
-    //             connection.query("INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES(?,?,?,?)", [res.firstName,res.lastName,res.role,res.manager], function (err, data) {
-    //                 if (err) throw err;
-    //                 console.table(data);
-    //             })
-    //         })
-    // }
+    function addEmployee(){
+        inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What it the first name of the employee you would like to add?",
+                    name: "firstName"
+                },
+                {
+                    type: "input",
+                    message: "What it the last name of the employee you would like to add?",
+                    name: "lastName"
+                },
+                {
+                    type: "input",
+                    message: "What it the role of the employee you would like to add?",
+                    name: "role"
+                },
+                {
+                    type: "input",
+                    message: "Who is the manager of the employee you would like to add?",
+                    name: "manager"
+                }
+            ]).then(function(res){
+                let firstName = res.firstName;
+                let lastName = res.lastName;
+                var role_id;
+                var manager_id;
+                let role = res.role;
+                let manager = res.manager;
+                convertNameId();
+                function convertNameId(){
+                    connection.query("SELECT id,title FROM role",(err, data)=>{
+                        if (err) throw err;
+                        // let stringData = JSON.stringify(data);
+                        // let parsedData = JSON.parse(stringData);
+                        for(let i = 0;i<data.length;i++){
+                            if(role===data[i].title){
+                                role_id = data[i].id;
+                                // console.log(role_id);
+                            }
+                        }
+                        connection.query("SELECT id,first_name FROM employee",(err, data)=>{
+                            if (err) throw err;
+                            // let stringData = JSON.stringify(data);
+                            // let parsedData = JSON.parse(stringData);
+                            for(let i = 0;i<data.length;i++){
+                                if(manager===data[i].first_name){
+                                    manager_id= data[i].id;
+                                    console.log(manager_id);
+                                }
+                            }
+                            var newEmployee = new Employee(firstName,lastName,role_id,manager_id);
+                            console.log(newEmployee)
+                        })
+                    })
+                }
+                // function addToData(role,manager){
+                //     connection.query("INSERT INTO employee (first_name,last_name,role_id,manager_id) VALUES(?,?,?,?)", [firstName,lastName,role_id,manager_id], function (err, data) {
+                //         if (err) throw err;
+                //         console.table(data);
+                // })
+                // }
+            })
+        }
+
+
+
 
   
